55794a83426e9a72d73ffd347fe378f2cf26aee5
kbrsrkl1 <kubranur216@gmail.com>
2024-12-31 18:32:49 +0300

    commit

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function startTimer(){ //Kronometreyi başlatan bir fonksiyon tanımlandı.
         elapsedTime = Date.now() - startTime //Şu anki zamanı (Date.now()) başlangıç zamanından (startTime) çıkarır ve geçen süreyi (elapsedTime) hesaplar.
         timerEl.textContent = formatTime(elapsedTime); //timerEl.textContent = elapsedTime;:elapsedTime değerini bir HTML elementine (timerEl) yazdırır.timerEl: DOM elementine referans olduğu anlaşılan bir değişken.
     },10) //10 (milisaniye): Güncelleme sıklığını belirler.Geçen süre hesaplanır ve ekrana yazdırılır.
+    startButtonEl.disabled = true;
+    stopButtonEl.disabled = false;
+    resetButtonEl.disabled = false;
 }
 
 
@@ -22,7 +25,10 @@ function formatTime(elapsedTime) {
     const miliseconds = Math.floor((elapsedTime % 1000) / 10);
     const seconds = Math.floor((elapsedTime % ( 1000 * 60)) / 1000);
     const minutes =  Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+    const hours = Math.floor((elapsedTime / (1000 * 60 * 60)));
     return (
+    (hours ? (hours > 9 ? hours : "0" + hours) : "00")
+    + "." + 
     (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")
     + "." + 
     (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") 
@@ -31,12 +37,23 @@ function formatTime(elapsedTime) {
     );
 }
 
+
+
+
 function stopTimer(){
-    console.log("stop");
+    clearInterval(timerInterval);
+    stopButtonEl.disabled = true;
+    startButtonEl.disabled = false;
+    resetButtonEl.disabled = false;
 }
 
 function resetTimer(){
-    console.log("reset");
+    clearInterval(timerInterval);
+    elapsedTime = 0;
+    timerEl.textContent = "00:00:00:00"
+    resetButtonEl.disabled = true;
+    startButtonEl.disabled = false;
+    stopButtonEl.disabled = false;
 }
 
 
